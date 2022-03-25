b2441a6c737d11e669bf11458ee6b94de6cbc6ee
habenzy <rstauss@burlingtoncodeacademy.com>
2022-03-25 16:17:58 -0400

    whoops

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 
 app.use(express.static('./public'))
 app.use(express.urlencoded({extended: true}))
-app.use(cors)
+app.use(cors())
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", '*');
   res.header("Access-Control-Allow-Credentials", true);
