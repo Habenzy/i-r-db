43c900a5264da46726999c9e02efef947e4e137e
habenzy <rstauss@burlingtoncodeacademy.com>
2022-03-25 16:44:13 -0400

    maybe?

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.static("./public"));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -27,13 +27,44 @@ app.use(function (req, res, next) {
 app.post("/i-r/dropbox/upload", (req, res) => {
   console.log("received HS request");
   console.log(req);
-  let body = req.body
+  let body = req.body;
 
   console.log("body data");
   console.log(body.data);
 
+  let b64Array = file_data.split("base64,");
+  let b64string = b64Array[1];
+  let b64FileType = b64Array[0];
+  console.log(b64Array);
+  console.log(b64FileType);
+  // Base64 Back to String
+  //  var byteCharacters = Buffer.from(b64string, 'base64');
+
+  const byteCharacters = atob(b64string);
+
+  //
+  const byteNumbers = new Array(byteCharacters.length);
+
+  for (let i = 0; i < byteCharacters.length; i++) {
+    byteNumbers[i] = byteCharacters.charCodeAt(i);
+  }
+
+  const byteArray = new Uint8Array(byteNumbers);
+
+  const blob = new Blob([byteArray], { type: "application/pdf" });
+
+  console.log(byteCharacters);
+  function toArrayBuffer(buf) {
+    const ab = new ArrayBuffer(buf.length);
+    const view = new Uint8Array(ab);
+    for (let i = 0; i < buf.length; ++i) {
+      view[i] = buf[i];
+    }
+    return ab;
+  }
+
   axios
-    .post("https://content.dropboxapi.com/2/files/upload", body.data, {
+    .post("https://content.dropboxapi.com/2/files/upload", blob, {
       headers: {
         Authorization:
           "Bearer sl.BEcdGHM5zTJhsRzAWrP6HLMVM2ZJUPZm-JZcqf2ApECb06ij2yyCWfpKYtAx-BoZeS1RJYulsf_JhkUW8p3B8jWhbB0Td5Cz4dNk_cRnK5VhHyl_rkX1MjvefVbCzpd6s6hjt4iLTqvQ",
@@ -64,36 +95,3 @@ app.post("/i-r/dropbox/upload", (req, res) => {
 });
 
 app.listen(port, () => console.log("Server Running"));
-
-// const objectType = '';
-
-// var b64Array = file_data.slice(0, file_data.length - 1).split("base64,");
-// let b64string = b64Array[1];
-// let b64FileType = b64Array[0];
-// console.log(b64Array);
-// console.log(b64FileType);
-// // Base64 Back to String
-// //  var byteCharacters = Buffer.from(b64string, 'base64');
-
-// const byteCharacters = atob(b64string);
-
-// //
-// const byteNumbers = new Array(byteCharacters.length);
-
-// for (let i = 0; i < byteCharacters.length; i++) {
-//   byteNumbers[i] = byteCharacters.charCodeAt(i);
-// }
-
-// const byteArray = new Uint8Array(byteNumbers);
-
-// const blob = new Blob([byteArray], { type: "application/pdf" });
-
-// console.log(byteCharacters);
-// function toArrayBuffer(buf) {
-//   const ab = new ArrayBuffer(buf.length);
-//   const view = new Uint8Array(ab);
-//   for (let i = 0; i < buf.length; ++i) {
-//     view[i] = buf[i];
-//   }
-//   return ab;
-// }
