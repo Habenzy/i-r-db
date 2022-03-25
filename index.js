5f66203c6ee8175525c46ce7e51a01b43c262156
habenzy <rstauss@burlingtoncodeacademy.com>
2022-03-25 16:10:18 -0400

    COOOOOOOORRRRRRRSSSS

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const port = process.env.PORT || 3000
 
 const app = express()
 
 app.use(express.static('./public'))
 app.use(express.urlencoded({extended: true}))
+app.use(cors)
 
 app.post('/i-r/dropbox/upload', (req, res) => {
   res.json({"status": "hit the endpoint"})
