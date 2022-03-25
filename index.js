09d579c9fc6487ce505d4b77f6a00c321e63136b
habenzy <rstauss@burlingtoncodeacademy.com>
2022-03-25 16:14:42 -0400

    coooors

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const app = express()
 app.use(express.static('./public'))
 app.use(express.urlencoded({extended: true}))
 app.use(cors)
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 
 app.post('/i-r/dropbox/upload', (req, res) => {
   res.json({"status": "hit the endpoint"})
